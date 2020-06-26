83c637706e4d1b76a43b9ec7da9aee7927501e94
CPPtrueCoder <andierodin@gmail.com>
2020-06-26 06:12:42 +0300

    Add functional for apply Todos in App with React Hooks: form was realized

--- App.js
+++ App.js
@@ -1,12 +1,42 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React , {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import {Navbar} from "./src/navbar";
+import {AddTodo} from "./src/AddTodo";
+import {Todo} from "./src/Todo";
 
 export default function App() {
+    const [todos,setTodos] = useState([]);
+    
+    
+    const addTodo=(title)=>{
+        // const newTodo={
+        //     id:Date.now().toString(),
+        //     title:title
+        // };
+      //  setTodos(todos.concat([newTodo]))
+      //   setTodos((prevTodos)=>{
+      //       return[
+      //           ...prevTodos,newTodo
+      //       ]
+      //   });
+        
+        setTodos(prev=> [...prev,
+            {   id:Date.now().toString(),
+            title}])
+        
+    };
+    
   return (
-    <View style={styles.container}>
+    <View >
      <Navbar title='Todo App'/>
+     <View style={styles.container}>
+     <AddTodo onSubmit={addTodo} />
+     <View>
+         {todos.map(item=> (<Todo todo={item} key={item.id}/>))}
+       
+     </View>
+     </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,7 +44,8 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-  
+  paddingHorizontal:30,
+      paddingVertical:20
   }
   
 });
--- src/AddTodo.js
+++ src/AddTodo.js
@@ -1,21 +1,51 @@
-import React from 'react'
-import {View,StyleSheet} from 'react-native'
+import React, {useState} from 'react'
+import {View, StyleSheet, TextInput, Button,Alert} from 'react-native'
 
-
-export const AddTodo= props=>{
-return (
-	<View>
+export const AddTodo = ({onSubmit}) => {
 	
-	</View>
-)
+	const [value, setValue] = useState ('');
+	
+	const pressHandler = () => {
+		if(value.trim()){
+			onSubmit (value);
+			setValue('')
+		}else{
+		Alert.alert('Название дела не может быть пустым')
+		}
+		
+	};
+	
+	return (
+		<View style={styles.block}>
+			<TextInput style={styles.input}
+			           onChangeText={setValue}
+			           value={value}
+			           placeholder="Введите задачу"
+			           autoCorrect={false}
+			           autoCapitalize='none'
+			           
+			
+			/>
+			<Button onPress={pressHandler} style={styles.button} title='Добавить'/>
+		</View>
+	)
 };
 
-
-const styles= StyleSheet.create({
+const styles = StyleSheet.create ({
 	
-	wrapper:{
-	
+	block: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		marginBottom: 15
+	},
+	input: {
+		padding: 10,
+		width: '70%',
+		borderStyle: 'solid',
+		borderBottomWidth: 2,
+		borderBottomColor: '#3949ab',
+		marginBottom: 10
 	}
-	
 	
 });
